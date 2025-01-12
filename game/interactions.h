2d79d0ec8052cd0c0668989aeda63d9cd9745e9b
Gaspard Obert <94133410+Nanjiifr@users.noreply.github.com>
2025-01-12 22:01:25 +0100

    Refactor interaction functions to improve type consistency and formatting

--- game/interactions.h
+++ game/interactions.h
@@ -4,23 +4,28 @@
 #include "creation.h"
 #include "salle.h"
 
+
+typedef struct Objet_s Objet ;
+typedef struct Personnage_s Personnage ;
+typedef enum obstacle_e obstacle ;
+
 // Fonction pour initialiser une bombe.
-void init_bombe(Objet* objet);
+void init_bombe(Objet* objet) ;
 
 // Fonction pour initialiser un téléporteur.
-void init_teleporteur(Objet* objet);
+void init_teleporteur(Objet* objet) ;
 
 // Fonction pour initialiser un rocher.
-void init_rocher(Objet* objet);
+void init_rocher(Objet* objet) ;
 
 // Fonction pour gérer l'interaction entre le personnage et un objet.
-void interaction_objet(Obstacle* obst, Salle* salle_act);
+void interaction_objet(obstacle* obst, salle* salle_act) ;
 
 // Fonction pour supprimer les salles adjacentes (utilisée par la bombe).
-void boom(Salle* salles, int* nombre_salles);
+void boom(salle* salles, int* nombre_salles) ;
 
 // Fonction pour téléporter le personnage dans une salle aléatoire.
-void teleporter_personnage(Personnage* personnage, Salle* salles, int nombre_salles)
+void teleporter_personnage(Personnage* personnage, salle* salles, int nombre_salles) ;
 
 // commentaire test
 
