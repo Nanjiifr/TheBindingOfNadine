ae478c58d498e830bc2ac1044b30f0847983d856
Gaspard Obert <94133410+Nanjiifr@users.noreply.github.com>
2025-02-24 19:59:48 +0100

    Conditions propres pour déplacement de cailloux, plus de bugs

--- game/input.c
+++ game/input.c
@@ -29,33 +29,113 @@ int getch() {
     return ch; 
 }
 
+bool count_rocks(Personnage* pers, int dir, int** m, int* n) {
+    if (dir == 2) {
+        int c = 1;
+        while (pers->x + c < 13 && m[pers->y][pers->x + c] == 1) {
+            c ++ ;
+        }
+        if (pers->x + c == 13) {
+            return false ;
+        } else if (m[pers->y][pers->x + c - 1] == 1) {
+            *n = c ;
+            return true ;
+        } else {
+            *n = -1 ;
+            return false ;
+        }
+    }
+    if (dir == 3) {
+        int c = 1;
+        while (pers->x - c >= 0 && m[pers->y][pers->x - c] == 1) {
+            c ++ ;
+        }
+        if (pers->x - c == -1) {
+            return false ;
+        } else if (m[pers->y][pers->x - c + 1] == 1) {
+            *n = c ;
+            return true ;
+        } else {
+            *n = -1 ;
+            return false ;
+        }
+    }
+    if (dir == 0) {
+        int c = 1;
+        while (pers->y - c >= 0 && m[pers->y - c][pers->x] == 1) {
+            c ++ ;
+        }
+        if (pers->y - c == -1) {
+            return false ;
+        } else if (m[pers->y - c + 1][pers->x] == 1) {
+            *n = c ;
+            return true ;
+        } else {
+            *n = -1 ;
+            return false ;
+        }
+    }
+    if (dir == 1) {
+        int c = 1;
+        while (pers->y + c < 9 && m[pers->y + c][pers->x] == 1) {
+            c ++ ;
+        }
+        if (pers->y + c == 9) {
+            return false ;
+        } else if (m[pers->y + c - 1][pers->x] == 1) {
+            *n = c ;
+            return true ;
+        } else {
+            *n = -1 ;
+            return false ;
+        }
+    }
+    return false ; //Ce cas ne devrait jamais arriver, c'est juste pour éviter les warnings (c'est moche à la compil)
+}
+
 void move (Personnage* pers, int dir, int** m) {
-    if (dir == 2 && pers->x < 11 && m[pers->y][pers-> x + 1] == 1) { //Est
-        pers->x += 1 ;
-        m[pers->y][pers-> x] = 0 ;
-        m[pers->y][pers-> x + 1] = 1 ;
-    } else if (dir == 2 && pers->x < 12 && m[pers->y][pers-> x + 1] != 1) {
-        pers->x += 1 ;
+    int n = 0;
+    if (count_rocks(pers, dir, m, &n)) {
+        if (dir == 2 && pers->x < 12) { // Est
+            pers->x += 1;
+            for (int i = 1; i < n; i++) {
+                m[pers->y][pers->x + i] = 1;
+            }
+            m[pers->y][pers->x] = 0;
+        }
+        if (dir == 3 && pers->x > 0) { // Ouest
+            pers->x -= 1;
+            for (int i = 1; i < n; i++) {
+                m[pers->y][pers->x - i] = 1;
+            }
+            m[pers->y][pers->x] = 0;
+        }
+        if (dir == 0 && pers->y > 0) { // Nord
+            pers->y -= 1;
+            for (int i = 1; i < n; i++) {
+                m[pers->y - i][pers->x] = 1;
+            }
+            m[pers->y][pers->x] = 0;
+        }
+        if (dir == 1 && pers->y < 8) { // Sud
+            pers->y += 1;
+            for (int i = 1 ; i < n; i++) {
+                m[pers->y + i][pers->x] = 1;
+            }
+            m[pers->y][pers->x] = 0;
+        }
+    } else {
+        if (dir == 2 && pers->x < 12 && n == -1) { // Est
+            pers->x += 1;
+        }
+        if (dir == 3 && pers->x  > 0 && n == -1) { // Ouest
+            pers->x -= 1;
+        }
+        if (dir == 0 && pers->y > 0 && n == -1) { // Nord
+            pers->y -= 1;
+        }
+        if (dir == 1 && pers->y < 8 && n == -1) { // Sud
+            pers->y += 1;
+        }
     }
-    if (dir == 3 && pers->x > 1 && m[pers->y][pers-> x - 1] == 1) { //Ouest
-        pers->x -= 1 ;
-        m[pers->y][pers-> x] = 0 ;
-        m[pers->y][pers-> x - 1] = 1 ;
-    } else if (dir == 3 && pers->x > 0 && m[pers->y][pers-> x - 1] != 1) {
-        pers->x -= 1 ;
-    }
-    if (dir == 0 && pers->y > 1 && m[pers->y - 1][pers-> x] == 1) { //Nord
-        pers->y -= 1 ;
-        m[pers->y][pers-> x] = 0 ;
-        m[pers->y - 1][pers-> x] = 1 ;
-    } else if (dir == 0 && pers->y > 0 && m[pers->y - 1][pers-> x] != 1) {
-        pers->y -= 1 ;
-    }
-    if (dir == 1 && pers->y < 7 && m[pers->y + 1][pers-> x] == 1) { //Sud
-        pers->y += 1 ;
-        m[pers->y][pers-> x] = 0 ;
-        m[pers->y + 1][pers-> x] = 1 ;
-    } else if (dir == 1 && pers->y < 8 && m[pers->y + 1][pers-> x] != 1) {
-        pers->y += 1 ;
-    } 
 }
